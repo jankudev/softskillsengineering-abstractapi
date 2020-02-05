dd2a4f534b27b40ebb43629e3829e2985f0b6445
Tomas Janku <janku.tomas@gmail.com>
2020-02-05 19:51:29 +0100

    Initial commit - SpringBoot blank app

---
+++ src/main/java/dev/janku/softskillsengineering/abstractapi/AbstractapiApplication.java
@@ -0,0 +1,13 @@
+package dev.janku.softskillsengineering.abstractapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/janku/softskillsengineering/abstractapi/AbstractapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.janku.softskillsengineering.abstractapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
